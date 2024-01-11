d7e157b6f3128bce8c37f0aebe28962b2cc5313c
calandradas <calandradudasccj49@gmail.com>
2024-01-11 15:05:41 +0800

    code commit first

---
+++ src/local-kity-minder/diy.js
@@ -0,0 +1,225 @@
+(function(){
+	var html = '';
+	html += '<a class="diy export" data-type="json">'+_lang_pack[_lang_default]['panels']['export']+'Json</a>',
+	html += '<a class="diy export" data-type="markdown">'+_lang_pack[_lang_default]['panels']['export']+'MD</a>',
+	html += '<a class="diy export" data-type="svg">'+_lang_pack[_lang_default]['panels']['export']+'SVG</a>',
+	html += '<a class="diy export" data-type="png">'+_lang_pack[_lang_default]['panels']['export']+'PNG</a>',
+	//html += '<a class="diy export" name="btn_export_km" data-type="json">'+_lang_pack[_lang_default]['panels']['export']+'KM</a>',
+	html += '<button class="diy input">'+_lang_pack[_lang_default]['panels']['import']+'<input type="file" id="fileInput"></button>';
+
+	$('.editor-title').append(html);
+
+	$('.diy').css({
+		//'height': '30px',
+		//'line-height': '30px',
+		'margin-top': '0px',
+		'float': 'left',
+		'background-color': '#fff',
+		'min-width': '62px',
+		'text-decoration': 'none',
+		'text-align': 'center',
+		'color': '#666',
+		'padding': '0 0 0 0',
+		'border': 'none',
+		'border-right': '1px solid #ccc',
+		'border-top': '1px solid #ccc',
+	});
+	$('.input').css({
+		'overflow': 'hidden',
+		'position': 'relative',
+	}).find('input').css({
+		cursor: 'pointer',
+		position: 'absolute',
+		top: 0,
+		bottom: 0,
+		left: 0,
+		right: 0,
+		display: 'inline-block',
+		opacity: 0
+	});
+	$('.export').css('cursor','pointer');
+
+	$('.export').click{
+		exportType = $(this).data('type');
+		editor.minder.exportData(exportType).then(function(content){
+			var blob = new Blob([content]);
+			switch(exportType){
+				case 'json':
+					console.log($.parseJSON(content));
+					break;
+				case 'png':
+					var arr = content.split(','), mime = arr[0].match(/:(.*?);/)[1],
+					  bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+					while (n--) {
+					  u8arr[n] = bstr.charCodeAt(n);
+					}
+					blob = new Blob([u8arr], {type: mime});
+					break;
+				default:
+					console.log(content);
+					break;
+			}
+			var url = URL.createObjectURL(blob);
+			var aLink = $this[0];
+			aLink.href = url;
+			aLink.download = $('#node_text1').text()+'.'+type;
+	}
+	/*
+	$(document).on('mouseover', '.export', function(event) {
+		// 链接在hover的时候生成对应数据到链接中
+		event.preventDefault();
+		var $this = $(this),
+				type = $this.data('type'),
+				exportType;
+		switch(type){
+			case 'km':
+				exportType = 'json';
+				break;
+			case 'md':
+				exportType = 'markdown';
+				break;
+			default:
+				exportType = type;
+				break;
+		}
+
+		editor.minder.exportData(exportType).then(function(content){
+			var blob = new Blob([content]);
+			switch(exportType){
+				case 'json':
+					console.log($.parseJSON(content));
+					break;
+				case 'png':
+					var arr = content.split(','), mime = arr[0].match(/:(.*?);/)[1],
+					  bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+					while (n--) {
+					  u8arr[n] = bstr.charCodeAt(n);
+					}
+					blob = new Blob([u8arr], {type: mime});
+					break;
+				default:
+					console.log(content);
+					break;
+			}
+			$this.css('cursor', 'pointer');
+			var url = URL.createObjectURL(blob);
+			var aLink = $this[0];
+			aLink.href = url;
+			aLink.download = $('#node_text1').text()+'.'+type;
+		});
+	}).on('mouseout', '.export', function(event) {
+		// 鼠标移开是设置禁止点击状态，下次鼠标移入时需重新计算需要生成的文件
+		event.preventDefault();
+		// $(this).css('cursor', 'not-allowed');
+	}).on('click', '.export', function(event) {
+		// 禁止点击状态下取消跳转
+		var $this = $(this);
+		if($this.css('cursor') == 'not-allowed'){
+			event.preventDefault();
+		}
+	});
+	*/
+
+	function get_datatime_by_yyyyMMddhhmmss_without_split() {
+		let d = new Date();
+		return d.getFullYear().toString() + (d.getMonth() + 1).toString() + d.getDate().toString() + d.getHours().toString() + d.getMinutes().toString() + d.getSeconds().toString();
+	}
+
+    function save_file(filename, content) {
+        let element = document.createElement('a');
+		filename = filename + '.km';
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+        element.setAttribute('download', filename);
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    }
+
+	// 导入
+	window.onload = function() {
+		// 导出事件
+		// 初始化时候，先import父框架的json数据
+		// 在iframe子页面中查找父页面元素
+
+		let parent = window.parent.document.getElementById('mindmap_diagram_json');
+		//alert(parent.value);
+		//$("[name='btn_export_km']").on("click", function(){
+		//	save_file(get_datatime_by_yyyyMMddhhmmss_without_split(), parent.value);
+		//});
+
+
+		let maintopic = _lang_pack[_lang_default]['maintopic'];
+		let data_json = '{"root":{"data":{"id":"cmhllt94xb40","created":1661683403686,"text":"' + maintopic + '"},"children":[]},"template":"default","theme":"fresh-blue","version":"1.4.33"}';
+		if(parent !=null && parent.value != ""){
+			data_json = parent.value;
+		}
+
+		editor.minder.importData('json', data_json).then(function(data){
+			console.log(data)
+			//$(fileInput).val('');
+		});
+		// == 初始化结束 == 
+
+		$('.export').css('cursor','default');
+		var fileInput = document.getElementById('fileInput');
+
+		// 导入部分的逻辑，支持导入md km json文件
+		fileInput.addEventListener('change', function(e) {
+			var file = fileInput.files[0],
+					// textType = /(md|km)/,
+					fileType = file.name.substr(file.name.lastIndexOf('.')+1);
+			console.log(file);
+			switch(fileType){
+				case 'md':
+					fileType = 'markdown';
+					break;
+				case 'km':
+				case 'json':
+					fileType = 'json';
+					break;
+				default:
+					console.log("File not supported!");
+					alert('Only .km .md .json file');
+					return;
+			}
+			var reader = new FileReader();
+			reader.onload = function(e) {
+				var content = reader.result;
+				editor.minder.importData(fileType, content).then(function(data){
+					console.log(data)
+					$(fileInput).val('');
+				});
+			}
+			reader.readAsText(file);
+		});
+
+		// 定时将最新数据转导到父框架上的input
+		setInterval(function(){
+			editor.minder.exportData('json').then(function(content){
+				parent.value = content;
+			});
+			editor.minder.exportData('png').then(function(content){
+				window.parent.document.getElementById('mindmap_diagram_png').value = content;
+			});
+		}, 500);
+
+		// 加入鼠标滚动放大缩小脚本
+		$(".minder-editor").on('mousewheel DOMMouseScroll', function(event) {
+			if(event.ctrlKey == true)
+			{
+				event.preventDefault();
+				if(event.originalEvent.wheelDelta > 0) {
+					 console.log('Down');
+					 editor.minder.execCommand('zoomIn');
+				 }else {
+					 console.log('Up');
+					 editor.minder.execCommand('zoomOut');
+				 }
+			}
+		});
+		
+	}
+
+
+})();
---
+++ src/resources.ts
@@ -0,0 +1,111 @@
+import joplin from 'api'
+import { v4 as uuidv4 } from 'uuid'
+import { tmpdir } from 'os'
+import { sep } from 'path'
+const fs = joplin.require('fs-extra')
+
+const Config = {
+    TempFolder: `${tmpdir}${sep}joplin-mindmap-plugin${sep}`,
+    DataImageRegex: /^data:image\/(?<extension>png|svg)(?:\+xml)?;base64,(?<blob>.*)/,
+    TitlePrefix: 'mindmap-',
+}
+
+export interface IDiagramOptions {
+    sketch?: boolean
+}
+
+function generateId() {
+    return uuidv4().replace(/-/g, '')
+}
+
+function buildTitle(json_resource_id: string): string {
+    return Config.TitlePrefix + json_resource_id
+}
+function parseTitle(title: string): any {
+    return title.replace(Config.TitlePrefix, '')
+}
+
+export function clearDiskCache(): void {
+    fs.rmdirSync(Config.TempFolder, { recursive: true })
+    fs.mkdirSync(Config.TempFolder, { recursive: true })
+}
+
+async function writeTempFile(name: string, data: string, filePath: string = null): Promise<string> {
+    const matches = data.match(Config.DataImageRegex)
+    if (!matches) {
+        throw new Error('Invalid image data')
+    }
+    if (!filePath) {
+        filePath = `${Config.TempFolder}${name}.${matches.groups.extension}`
+    }
+    await fs.writeFile(filePath, matches.groups.blob, 'base64')
+    return filePath
+}
+
+
+async function writeJsonFile(name: string, data: string, filePath: string = null): Promise<string> {
+    if (!filePath) {
+        filePath = `${Config.TempFolder}${name}.json`
+    }
+    await fs.writeFile(filePath, data)
+    return filePath
+}
+
+export async function getDiagramResource(diagramId: string): Promise<{ body: string, data_json:string }> {
+    let resourceProperties = await joplin.data.get(['resources', diagramId])
+    let resourceData = await joplin.data.get(['resources', diagramId, 'file'])
+
+    let data_json: string = ""
+    try {
+        data_json = parseTitle(resourceProperties.title)
+    } catch (e) {
+        console.warn('getDiagramResource - json resource ID parsing failed:', e)
+    }
+
+    // let resourceData_json = await joplin.data.get(['resources', json_resource_id, 'file'])
+    // let data_json = new TextDecoder().decode(resourceData_json.body);
+    console.log('getDiagramResource', resourceProperties, resourceData)
+    if (!resourceData.contentType.startsWith('image')) {
+        throw new Error('Invalid resource content type. The resource must be an image')
+    }
+    console.log('getDiagramResource diagramId', diagramId);
+    console.log('getDiagramResource json', data_json);
+    return {
+        body: `data:${resourceData.contentType};base64,${Buffer.from(resourceData.body).toString('base64')}`,
+        data_json: data_json
+    }
+}
+
+export async function createDiagramResource(data: string, data_json:string): Promise<string> {
+    let diagramId = generateId()
+    // let json_resource_id = generateId()
+
+    let filePath = await writeTempFile(diagramId, data)
+    // let filePath_json = await writeJsonFile(json_resource_id, data_json)
+    let createdResource = await joplin.data.post(['resources'], null, { id: diagramId, title: buildTitle(data_json) }, [{ path: filePath }])
+    // let createdResource_json = await joplin.data.post(['resources'], null, { id: json_resource_id, title: "mindmap-data-json"}, [{path: filePath_json}])
+
+    console.log('createResource diagramId', diagramId);
+    console.log('createResource json', data_json);
+    return diagramId
+}
+
+export async function updateDiagramResource(diagramId: string, data: string, data_json:string ): Promise<string> {
+    let newDiagramId = generateId()
+    // let new_json_resource_id = generateId()
+
+    let filePath = await writeTempFile(newDiagramId, data)
+    // let filePath_json = await writeJsonFile(new_json_resource_id, data_json)
+    let createdResource = await joplin.data.post(['resources'], null, { id: newDiagramId, title: buildTitle(data_json) }, [{ path: filePath }])
+    // let createdResource_json = await joplin.data.post(['resources'], null, { id: new_json_resource_id, title: "mindmap-data-json"}, [{path: filePath_json}])
+    // let createdResource_json = await joplin.data.post(['notes'], null, { id: newDiagramId, body: data_json })
+    // I will not delete the previous resource just in case it has been copied in another note
+    // await joplin.data.delete(['resources', diagramId])
+    // console.log('createdResource', createdResource)
+    return newDiagramId
+}
+
+export async function isDiagramResource(diagramId: string): Promise<boolean> {
+    let resourceProperties = await joplin.data.get(['resources', diagramId])
+    return resourceProperties.title.startsWith(Config.TitlePrefix)
+}
